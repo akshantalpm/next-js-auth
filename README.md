a558d4dc7ea2a07be6e8608e8d39befed3121855
Pooja Bongirwar <pooja.bongirwar@deserve.com>
2021-02-04 18:23:21 +0530

    Allow user to sigin with apple login

---
+++ README.md
@@ -0,0 +1,1 @@
+### Next JS Auth for Sign In With Apple
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,56 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+const providers = [
+  Providers.GitHub({
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET
+  }),
+  Providers.Apple({
+   clientId: process.env.CLIENT_ID,
+    clientSecret: {
+      appleId: process.env.CLIENT_ID,
+      teamId: "3JRGPHC3AW",
+      privateKey: process.env.access_key,
+      keyId: process.env.key_id,
+    }
+  })
+]
+
+const callbacks = {}
+
+/**
+ * @param  {object} session      Session object
+ * @param  {object} user         User object    (if using database sessions)
+ *                               JSON Web Token (if not using database sessions)
+ * @return {object}              Session that will be returned to the client
+ */
+
+
+
+callbacks.jwt = async (token, user, account, profile) => {
+  if (token) {
+    return Promise.resolve({ ...token, ...user, ...account, ...profile });
+  }
+}
+
+callbacks.session = async (session, user, sessionToken) => {
+  console.log(user)
+  return Promise.resolve({ ...session, ...user, ...sessionToken })
+}
+
+
+const options = {
+  providers,
+  session: {
+    jwt: true
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET
+  },
+  callbacks
+}
+
+
+
+export default (req, res) => NextAuth(req, res, options)
